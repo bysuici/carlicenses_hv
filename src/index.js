5848034466d780cac0b33d282c5f4a47450f3c04
bysuici <eduardx62@outlook.com>
2026-03-02 12:28:01 -0600

    Endpoints Fan ID

--- src/index.js
+++ src/index.js
@@ -30,12 +30,22 @@ app.post('/eventRcv', async (request, response) => {
         }
 
         try {
+            // COVIA Endpoint Placas
             const backendCoviaResponse = await axios.post(`https://api-covia.okip.com.mx/plate-event`, request.body, {
                 headers: {
                     'Content-Type': 'application/json'
                 }
             })
+
+            // Bitacora Endpoint Placas
             const backendBitacoraResponse = await axios.post(`https://api-bitacora.okip.com.mx/api/event/plates`, request.body, {
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            })
+
+            // Fan ID Endpoint Placas
+            const backendFanIDResponse = await axios.post(`https://fanid-okip.okip.com.mx/api/v1/hikvision/events/plates/listener`, request.body, {
                 headers: {
                     'Content-Type': 'application/json'
                 }
@@ -55,35 +65,63 @@ app.post('/eventRcv', async (request, response) => {
 
 // NUEVOS EVENTOS
 app.post('/new/events', (request, response) => {
-    try {
-        console.log('==================================================')
-        console.log(`[${new Date().toISOString()}] Evento Recibido de Person:`)
-        console.log('==================================================')
+    // try {
+    //     console.log('==================================================')
+    //     console.log(`[${new Date().toISOString()}] Evento Recibido de Person:`)
+    //     console.log('==================================================')
 
-        console.log('HEADERS:')
-        console.dir(request.headers, { depth: null, colors: true })
+    //     console.log('HEADERS:')
+    //     console.dir(request.headers, { depth: null, colors: true })
 
-        console.log('\nBODY:')
-        console.dir(request.body, { depth: null, colors: true })
+    //     console.log('\nBODY:')
+    //     console.dir(request.body, { depth: null, colors: true })
 
-        const events = request.body?.params?.events
+    //     const events = request.body?.params?.events
 
-        if (events) {
-            console.log('\nEVENTS:')
-            console.dir(events, { depth: null, colors: true })
-        } else {
-            console.log('\nEVENTS: No events in params')
-        }
+    //     if (events) {
+    //         console.log('\nEVENTS:')
+    //         console.dir(events, { depth: null, colors: true })
+    //     } else {
+    //         console.log('\nEVENTS: No events in params')
+    //     }
 
-        console.log('==================================================\n\n')
-    } catch (error) {
-        console.error('Error procesando el log:', error.message)
-    }
+    //     console.log('==================================================\n\n')
+    // } catch (error) {
+    //     console.error('Error procesando el log:', error.message)
+    // }
     response.status(200).send({ status: 'ok', message: 'Event received' });
 })
 
 // TORNIQUETE
 app.post('/event/200518', async (request, response) => {
+    const events = request.body?.params?.events[0]
+
+    console.log('==================================================')
+    console.log(`[${new Date().toISOString()}] Evento Recibido de Person - Torniquete:`)
+    console.log('==================================================')
+
+    console.log('\nBODY:')
+    console.dir(request.body, { depth: null, colors: true })
+    console.log('==================================================\n\n')
+
+    try {
+        await axios.post(`https://api-bitacora.okip.com.mx/api/event/torniquete/200518`, events, { headers: { 'Content-Type': 'application/json' } })
+        response.status(200).send({ status: 'ok', message: 'Event received' })
+    } catch (axiosError) {
+        console.error('Error enviando evento al backend principal:', axiosError.message)
+    }
+
+    // Fan ID Endpoint Torniquetes
+    try {
+        await axios.post('https://fanid-okip.okip.com.mx/api/v1/hikvision/events/torniquete/listener', events, { headers: { 'Content-Type': 'application/json' } })
+        response.status(200).send({ status: 'ok', message: 'Event received' })
+    } catch (error) {
+        console.error('Error enviando evento al backend FanID:', error.message)
+    }
+})
+
+// BIOMETRICO
+app.post('/event/196893', async (request, response) => {
     const events = request.body?.params?.events[0]
     try {
         // const backendCoviaResponse = await axios.post(`https://api-covia.okip.com.mx/plate-event`, request.body, {
@@ -92,80 +130,66 @@ app.post('/event/200518', async (request, response) => {
         //     }
         // })
 
-        await axios.post(`https://api-bitacora.okip.com.mx/api/event/torniquete/200518`, events, { headers: { 'Content-Type': 'application/json' } })
-
-        response.status(200).send({ status: 'ok', message: 'Event received' })
+        await axios.post(`https://api-bitacora.okip.com.mx/api/event/torniquete/196893`, events, { headers: { 'Content-Type': 'application/json' } })
     } catch (axiosError) {
         console.error('Error enviando evento al backend principal:', axiosError.message)
     }
-})
 
-// BIOMETRICO
-app.post('/event/196893', async (request, response) => {
+    // Fan ID Endpoint Biometrico
     try {
-        const events = request.body?.params?.events[0]
-        try {
-            // const backendCoviaResponse = await axios.post(`https://api-covia.okip.com.mx/plate-event`, request.body, {
-            //     headers: {
-            //         'Content-Type': 'application/json'
-            //     }
-            // })
-
-            await axios.post(`https://api-bitacora.okip.com.mx/api/event/torniquete/196893`, events, { headers: { 'Content-Type': 'application/json' } })
-        } catch (axiosError) {
-            console.error('Error enviando evento al backend principal:', axiosError.message)
-        }
-
+        await axios.post('https://fanid-okip.okip.com.mx/api/v1/hikvision/events/access/listener', events, { headers: { 'Content-Type': 'application/json' } })
         response.status(200).send({ status: 'ok', message: 'Event received' })
     } catch (error) {
-        console.error('Error procesando el log:', error.message)
-        response.status(500).send({ status: 'error', message: 'Internal error' })
+        console.error('Error enviando evento al backend FanID:', error.message)
     }
 })
 
 // PUERTAS POR HUELLA
 app.post('/event/197127', async (request, response) => {
+    const events = request.body?.params?.events[0]
     try {
-        const events = request.body?.params?.events[0]
-        try {
-            // const backendCoviaResponse = await axios.post(`https://api-covia.okip.com.mx/plate-event`, request.body, {
-            //     headers: {
-            //         'Content-Type': 'application/json'
-            //     }
-            // })
+        // const backendCoviaResponse = await axios.post(`https://api-covia.okip.com.mx/plate-event`, request.body, {
+        //     headers: {
+        //         'Content-Type': 'application/json'
+        //     }
+        // })
 
-            await axios.post(`https://api-bitacora.okip.com.mx/api/event/doors/197127`, events, { headers: { 'Content-Type': 'application/json' } })
-        } catch (axiosError) {
-            console.error('Error enviando evento al backend principal:', axiosError.message)
-        }
+        await axios.post(`https://api-bitacora.okip.com.mx/api/event/doors/197127`, events, { headers: { 'Content-Type': 'application/json' } })
+    } catch (axiosError) {
+        console.error('Error enviando evento al backend principal:', axiosError.message)
+    }
 
+    // Fan ID Endpoint Biometrico
+    try {
+        await axios.post('https://fanid-okip.okip.com.mx/api/v1/hikvision/events/doors/listener', events, { headers: { 'Content-Type': 'application/json' } })
         response.status(200).send({ status: 'ok', message: 'Event received' })
     } catch (error) {
-        console.error('Error procesando el log:', error.message)
-        response.status(500).send({ status: 'error', message: 'Internal error' })
+        console.error('Error enviando evento al backend FanID:', error.message)
     }
+
 })
 
 // ABRIR PUERTAS CON TARJETA
 app.post('/event/198914', async (request, response) => {
+    const events = request.body?.params?.events[0]
     try {
-        const events = request.body?.params?.events[0]
-        try {
-            // const backendCoviaResponse = await axios.post(`https://api-covia.okip.com.mx/plate-event`, request.body, {
-            //     headers: {
-            //         'Content-Type': 'application/json'
-            //     }
-            // })
+        // const backendCoviaResponse = await axios.post(`https://api-covia.okip.com.mx/plate-event`, request.body, {
+        //     headers: {
+        //         'Content-Type': 'application/json'
+        //     }
+        // })
 
-            await axios.post(`https://api-bitacora.okip.com.mx/api/event/doors/198914`, events, { headers: { 'Content-Type': 'application/json' } })
-        } catch (axiosError) {
-            console.error('Error enviando evento al backend principal:', axiosError.message)
-        }
+        await axios.post(`https://api-bitacora.okip.com.mx/api/event/doors/198914`, events, { headers: { 'Content-Type': 'application/json' } })
+    } catch (axiosError) {
+        console.error('Error enviando evento al backend principal:', axiosError.message)
+    }
 
+    // Fan ID Endpoint Puertas por Tarjeta
+    try {
+        await axios.post('https://fanid-okip.okip.com.mx/api/v1/hikvision/events/cards/listener', events, { headers: { 'Content-Type': 'application/json' } })
         response.status(200).send({ status: 'ok', message: 'Event received' })
     } catch (error) {
-        console.error('Error procesando el log:', error.message)
-        response.status(500).send({ status: 'error', message: 'Internal error' })
+        console.error('Error enviando evento al backend FanID:', error.message)
     }
 })
 
